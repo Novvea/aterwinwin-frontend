81402dc303ac52510fee660aee1c494bec2315bd
Novvea <anna.c.novak@gmail.com>
2021-03-10 15:43:36 +0100

    Create interface for new user

--- src/shared/interface/Interface.ts
+++ src/shared/interface/Interface.ts
@@ -1,5 +1,11 @@
 export interface i_loginCredentials {
   username: string;
   password: string;
-  isAuthenticated: boolean; 
+  isAuthenticated: boolean;
+}
+
+export interface i_createNewUser {
+  username: string,
+  password: string,
+  /* age?: number */
 }
