bfe103a61881f2d9c63ac6c49cc9b2eced1188a3
Novvea <anna.c.novak@gmail.com>
2021-03-11 14:49:35 +0100

    Add inputs to additemview

--- src/view/additem/AddItemView.tsx
+++ src/view/additem/AddItemView.tsx
@@ -5,6 +5,13 @@ export const AddItemView = () => {
   return (
     <div className={styles.wrapper}>
       <h1>Lägg till objekt</h1>
+      <div>Ladda upp bild</div>
+      <label> Objekttyp:
+        <input type="text" required />
+      </label>
+      <label> Kategori:
+        <input type="text" required />
+      </label>
     </div>
   )
 }
