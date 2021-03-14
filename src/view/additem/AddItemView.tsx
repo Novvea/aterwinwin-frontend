4451fd87a966cb3ca960bac2f3dcbc5830a61042
Novvea <anna.c.novak@gmail.com>
2021-03-14 09:28:49 +0100

    Be able to add photos by cloudinary

--- src/view/additem/AddItemView.tsx
+++ src/view/additem/AddItemView.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import BackendAPIService from '../../shared/api/service/BackendAPIService'
 import styles from './AddItemView.module.css'
+import axios from 'axios'
 
 export const AddItemView = () => {
   const [addItemFormData, setAddItemFormData] = useState({
     itemname: '',
-    category: ''
+    category: '',
+    imageUrl: ''
   })
+
+  const handleChangeImageFile = async (event: any) => {
+    if (event.target.files) {
+      const formData = new FormData()
+      formData.append('file', event.target.files[0])
+      formData.append('upload_preset', 'xjrgcel9')
+
+      axios.post('https://api.Cloudinary.com/v1_1/novve/image/upload/ ', formData)
+        .then(response => { console.log('aa', response.data); setAddItemFormData({ ...addItemFormData, imageUrl: response.data.url }) })
+        /* .then(response => setAddItemFormData({ ...addItemFormData, imageUrl: response.data.url })) */
+        .catch(err => console.log(err))
+    }
+  }
 
   const addNewItem = async () => {
     try {
@@ -20,7 +35,9 @@ export const AddItemView = () => {
   return (
     <div className={styles.wrapper}>
       <h1>Lägg till objekt</h1>
-      <div>Ladda upp bild</div>
+      <label>Ladda upp bild:
+        <input type="file" onChange={handleChangeImageFile} />
+      </label>
       <label> Objekttyp:
         <input type="text" required onChange={(event) => setAddItemFormData({ ...addItemFormData, itemname: event.target.value })} />
       </label>
@@ -28,6 +45,9 @@ export const AddItemView = () => {
         <input type="text" required onChange={(event) => setAddItemFormData({ ...addItemFormData, category: event.target.value })} />
       </label>
       <button onClick={() => addNewItem()}>Spara</button>
+
+      <p>The resulting image will be displayed here</p>
+      {addItemFormData.imageUrl && (<img src={addItemFormData.imageUrl} alt={addItemFormData.itemname} />)}
     </div>
   )
 }
